5dc19b76d8cbe7acbd4a40d91f005343fc92023c
shivam-724 <shivamsrivastavaic@gmail.com>
2024-07-10 15:06:53 +0530

    FrontEnd Initialised

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			
+		</div>
+  )
+}
